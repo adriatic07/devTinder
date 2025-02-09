b1e7fddc254de3086b617d2bc441c6402ae52af5
Aniket Jain <jaina290@gmail.com>
2025-02-09 14:28:19 +0530

    Created /signup to post user data to DB

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const { connectDb } = require("./config/Database");
+const User = require("./models/user");
 const app = express();
+
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Vishal",
+    lastName: "Jain",
+    age: 26,
+    gender: "Male",
+    emailId: "Vishal@jain.com",
+    password: "vishal@123",
+  });
+
+  await user.save();
+
+  res.send("User successfully added!");
+});
 
 connectDb()
   .then(() => {
